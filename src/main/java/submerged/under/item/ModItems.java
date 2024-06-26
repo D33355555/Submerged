327968d6451bab1777bf632da4bd82ed44b04b7e
soooy <alejandroraylicayan@gmail.com>
2024-06-26 21:55:25 +0100

    added essential files and modid finally

---
+++ src/main/java/submerged/under/item/ModItems.java
@@ -0,0 +1,19 @@
+package net.submerged.under.item;
+
+//import classes
+
+
+
+public class ModItems
+{
+
+    public static void registerModItems{} {
+
+
+
+} //register mod items here
+
+
+
+} //end class
+
