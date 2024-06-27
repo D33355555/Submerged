e10cdc01f8c6d12518ef76f32e6a57b602a155e6
soooy <alejandroraylicayan@gmail.com>
2024-06-27 20:40:30 +0100

    Registered the Kraken Tooth item and added a custom submerged item group

--- src/main/java/submerged/under/Submerged.java
+++ src/main/java/submerged/under/Submerged.java
@@ -4,17 +4,19 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import submerged.under.item.ModItemGroups;
+import submerged.item.ModItems;
+import submerged.item.ModItemsGroups;
 
 public class Submerged implements ModInitializer {
 
 	public static final String MOD_ID = "submerged";
 
-    public static final Logger LOGGER = LoggerFactory.getLogger("submerged");
+	public static final Logger LOGGER = LoggerFactory.getLogger("submerged");
 
 	@Override
 	public void onInitialize() {
-		ModItemGroups.registerItemGroups();
+		ModItemsGroups.registerItemGroups();
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 
 
