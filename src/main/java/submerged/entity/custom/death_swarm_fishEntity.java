76fe9073c16f9c69ca980baa4719c5c8218f6330
HexDawn <162615314+HexDawn@users.noreply.github.com>
2024-07-05 16:46:34 +0100

    - Add Death Swarm Fish Model, Textures and Animations - Add Partial Code for Death Swarm Fish

---
+++ src/main/java/submerged/entity/custom/death_swarm_fishEntity.java
@@ -0,0 +1,15 @@
+package submerged.entity.custom;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.world.World;
+
+public class death_swarm_fishEntity extends PathAwareEntity
+{
+
+    public death_swarm_fishEntity(EntityType<? extends PathAwareEntity> entityType, World world)
+    {
+        super(entityType, world);
+    }
+
+}// End Class
